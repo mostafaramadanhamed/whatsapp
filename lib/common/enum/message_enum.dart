5b449ac2b3e3d80776f189bad5fc628817dd5fee
mostafaramadanhamed <mostafaramadanhamed010@gmail.com>
2023-03-12 11:39:20 +0200

    add extension convert message

--- lib/common/enum/message_enum.dart
+++ lib/common/enum/message_enum.dart
@@ -10,3 +10,21 @@ enum MessageEnum{
 
  const MessageEnum(this.type);
 }
+extension ConvertMessage on String{
+  MessageEnum toEnum(){
+    switch(this){
+      case 'audio':
+        return MessageEnum.audio;
+        case 'image':
+        return MessageEnum.image;
+        case 'video':
+        return MessageEnum.video;
+        case 'text':
+        return MessageEnum.text;
+        case 'gif':
+        return MessageEnum.gif;
+      default:
+        return MessageEnum.text;
+    }
+  }
+}

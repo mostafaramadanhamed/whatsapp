c13092798a8dbffe29b2eff8afdafd737294eee8
mostafaramadanhamed <mostafaramadanhamed010@gmail.com>
2023-06-12 10:45:44 +0300

    edit message model

--- lib/models/massege_model.dart
+++ lib/models/massege_model.dart
@@ -9,9 +9,9 @@ class Message {
   final DateTime timeSent;
   final String messageId;
   final bool isSeen;
-  // final String repliedMessage;
-  // final String repliedTo;
-  // final MessageEnum repliedMessageType;
+  final String repliedMessage;
+ final String repliedTo;
+  final MessageEnum repliedMessageType;
 
   Message({
     required this.senderId,
@@ -21,9 +21,9 @@ class Message {
     required this.timeSent,
     required this.messageId,
     required this.isSeen,
-    // required this.repliedMessage,
-    // required this.repliedTo,
-    // required this.repliedMessageType,
+     required this.repliedMessage,
+     required this.repliedTo,
+     required this.repliedMessageType,
   });
 
   Map<String, dynamic> toMap() {
@@ -35,9 +35,9 @@ class Message {
       'timeSent': timeSent.millisecondsSinceEpoch,
       'messageId': messageId,
       'isSeen': isSeen,
-      // 'repliedMessage': repliedMessage,
-      // 'repliedTo': repliedTo,
-      // 'repliedMessageType': repliedMessageType.type,
+       'repliedMessage': repliedMessage,
+     'repliedTo': repliedTo,
+       'repliedMessageType': repliedMessageType.type,
     };
   }
 
@@ -50,9 +50,9 @@ class Message {
       timeSent: DateTime.fromMillisecondsSinceEpoch(map['timeSent']),
       messageId: map['messageId'] ?? '',
       isSeen: map['isSeen'] ?? false,
-      // repliedMessage: map['repliedMessage'] ?? '',
-      // repliedTo: map['repliedTo'] ?? '',
-      // repliedMessageType: (map['repliedMessageType'] as String).toEnum(),
+       repliedMessage: map['repliedMessage'] ?? '',
+       repliedTo: map['repliedTo'] ?? '',
+       repliedMessageType: (map['repliedMessageType'] as String).toEnum(),
     );
   }
 }

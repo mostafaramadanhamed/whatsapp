b6baafec4c4036210e27566774fd46b3347bcc39
mostafaramadanhamed <mostafaramadanhamed010@gmail.com>
2023-06-15 11:33:46 +0300

    add status repository

--- lib/features/status/repository/status_repository.dart
+++ lib/features/status/repository/status_repository.dart
@@ -3,26 +3,99 @@ import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_contacts/flutter_contacts.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:uuid/uuid.dart';
+import 'package:whatsapp/common/repository/firebasre_repository.dart';
 import 'package:whatsapp/common/utils/utils.dart';
+import 'package:whatsapp/models/status_model.dart';
+import 'package:whatsapp/models/user_model.dart';
 
-class StatusRepository{
-final FirebaseFirestore firestore;
-final FirebaseAuth auth;
-final ProviderRef ref;
-  StatusRepository({required this.firestore,required this.auth,required this.ref,});
+final statusProvider=Provider((ref) => StatusRepository(firestore: FirebaseFirestore.instance, auth: FirebaseAuth.instance, ref: ref,),);
+
+class StatusRepository {
+  final FirebaseFirestore firestore;
+  final FirebaseAuth auth;
+  final ProviderRef ref;
+  StatusRepository({
+    required this.firestore,
+    required this.auth,
+    required this.ref,
+  });
 
   void updateStatus({
-required String userName,
-required String profilePic,
-required String phoneNumber,
+    required String userName,
+    required String profilePic,
+    required String phoneNumber,
     required File statusImage,
     required BuildContext context,
-})async{
-    try{}
-        catch(ex){
-      showSnackBar(context: context, content: ex.toString());
+  }) async {
+    try {
+      var statusId = const Uuid().v1();
+      String uid = auth.currentUser!.uid;
+      String imageUrl = await ref
+          .read(commonFirebaseStorageRepositoryProvider)
+          .storeFileToFirebase('/status/$statusId', statusImage);
+      List<Contact> contacts = [];
+      if (await FlutterContacts.requestPermission()) {
+        contacts = await FlutterContacts.getContacts(
+          withProperties: true,
+        );
+      }
+      List<String> uidWhoCanSee = [];
+      for (int index = 0; index < contacts.length; index++) {
+        var userDataFirebase = await firestore
+            .collection('users')
+            .where(
+              'phoneNumber',
+              isEqualTo: contacts[index].phones[0].number.replaceAll(
+                    ' ',
+                    '',
+                  ),
+            )
+            .get();
+        if (userDataFirebase.docs.isNotEmpty) {
+          var userData = UserModel.fromMap(userDataFirebase.docs[0].data());
+          uidWhoCanSee.add(userData.uid);
         }
-  }
+        List<String> statusImageUrls = [];
+        var statusSnapShot = await firestore
+            .collection('status')
+            .where(
+              'uid',
+              isEqualTo: auth.currentUser!.uid,
+            )
+            .get();
+        if (statusSnapShot.docs.isNotEmpty) {
+          StatusModel status =
+              StatusModel.fromMap(statusSnapShot.docs[0].data());
+          statusImageUrls = status.photoUrl;
+          statusImageUrls.add(imageUrl);
+          await firestore
+              .collection('status')
+              .doc(statusSnapShot.docs[0].id)
+              .update({
+            'photoUrl': statusImageUrls,
+          });
+          return;
+        } else {
+          statusImageUrls = [imageUrl];
+        }
+        StatusModel status = StatusModel(
+          uid: uid,
+          userName: userName,
+          profilePic: profilePic,
+          phoneNumber: phoneNumber,
+          statusId: statusId,
+          photoUrl: statusImageUrls,
+          createdAt:DateTime.now() ,
+          whoCanSee:uidWhoCanSee,
+        );
+        await firestore.collection('status').doc().set(status.toMap());
 
+      }
+    } catch (ex) {
+      showSnackBar(context: context, content: ex.toString());
+    }
+  }
 }
